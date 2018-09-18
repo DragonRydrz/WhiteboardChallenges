dcbf2437cf2ea16de722b0cb31bdb95a6993c093
AJ Genung <DragonRydrz@me.com>
2018-09-18 12:33:37 -0400

    Balanced Brackets tests completed

--- BalancedBrackets/BalancedBrackets.spec.js
+++ BalancedBrackets/BalancedBrackets.spec.js
@@ -1,1 +1,20 @@
 const { balancedBrackets } = require('./BalancedBrackets');
+
+describe('Balanced Brackets Test Suite', () => {
+  it('should return false', () => {
+    const results = balancedBrackets('({[({[]})]})');
+    expect(results).toBe(false);
+  });
+  it('should return false', () => {
+    const results = balancedBrackets('[]{}()');
+    expect(results).toBe(true);
+  });
+  it('should return false', () => {
+    const results = balancedBrackets('[{[()]}]');
+    expect(results).toBe(true);
+  });
+  it('should return false', () => {
+    const results = balancedBrackets('[({}}]');
+    expect(results).toBe(false);
+  });
+});
