fdd791be4e2af34cc03a1d540124dfec801f92ed
AJ Genung <DragonRydrz@me.com>
2018-09-25 21:57:53 -0400

    bubble sort completed

--- Sorting/Sorting.js
+++ Sorting/Sorting.js
@@ -2,15 +2,37 @@
   Impliment a bubble sort algorithm.
 */
 
+function swap(arr, i, j) {
+  const temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+}
+
 const bubble = arr => {
   // Start at the beginning of the array, compare/swap to move
   // the larger number to the end of the array.
+  let swaps = false;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        swap(arr, j, j + 1);
+        swaps = true;
+      }
+    }
+    if (!swaps) {
+      break;
+    } else {
+      swaps = false;
+    }
+  }
+  return arr;
 };
 
 const selection = arr => {
   // Start at the beginning, loop thought the raminder of the array,
   // to find the lowest avaliable value, and swap it into the array
   // where it belongs. O(n^2)
+  return arr;
 };
 
 const insertion = arr => {
@@ -18,12 +40,14 @@ const insertion = arr => {
   // it to each element before it, swapping with the value before
   // if it is smaller.  Once it is where it belongs, move on to
   // the next value in the array.
+  return arr;
 };
 
 const quick = arr => {
   // Pick a pivot, place all elements lower then it to the left,
   // higher elements to the right.  Call recursivly on each subarray.
   // concat together when only one element in the array.
+  return arr;
 };
 
 module.exports = {
