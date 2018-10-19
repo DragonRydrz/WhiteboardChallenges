2a651c0d9e2627610e5223435aa29f80fb532529
AJ Genung <DragonRydrz@me.com>
2018-10-19 17:29:21 -0400

    Climbing Stairs adds 2 more test cases

--- ClimbingStairs/ClimbingStairs.spec.js
+++ ClimbingStairs/ClimbingStairs.spec.js
@@ -7,4 +7,10 @@ describe('Climbing Stairs test suite', () => {
   it('should return 4', () => {
     expect(climbingStairs(3)).toBe(4);
   });
+  it('should return 274', () => {
+    expect(climbingStairs(10)).toBe(274);
+  });
+  it('should return 53798080', () => {
+    expect(climbingStairs(30)).toBe(53798080);
+  });
 });
