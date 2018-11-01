f8195328ba1a58a407417fabebe3ab9bd6f1b18c
AJ Genung <DragonRydrz@me.com>
2018-10-31 22:53:29 -0400

    added test to Merging Two Packages

--- MergingTwoPackages/MergingTwoPackages.spec.js
+++ MergingTwoPackages/MergingTwoPackages.spec.js
@@ -10,4 +10,7 @@ describe('Merging Two Packages Testing Suite', () => {
   it('should return `[ 3, 1 ]`', () => {
     expect(mergingTwoPackages([2, 4, 2, 8, 6, 1], 12)).toMatchObject([3, 1]);
   });
+  it('should return `[ 3, 0 ]`', () => {
+    expect(mergingTwoPackages([0, 2, 3, 1, 4, 6, 12, 54], 1));
+  });
 });
