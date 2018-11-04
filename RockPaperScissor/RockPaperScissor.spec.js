775657c14842c867640d8425b44d99a533aa93bb
AJ Genung <DragonRydrz@me.com>
2018-11-04 09:35:31 -0500

    fixed typo

--- RockPaperScissor/RockPaperScissor.spec.js
+++ RockPaperScissor/RockPaperScissor.spec.js
@@ -1,7 +1,7 @@
-const rockPaperScissor = require('./RockPaperScissor');
+const { rockPaperScissors } = require('./rockPaperScissor');
 
 describe('Rock Paper Scissor testing suite', () => {
   it('should return an array', () => {
-    expect(Array.isArray(rockPaperScissor(1))).toBeTruthy();
+    expect(Array.isArray(rockPaperScissors(1))).toBeTruthy();
   });
 });
