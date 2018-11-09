058bc3bff1ab3e6cafad69d1ef253cbc610c1699
AJ Genung <DragonRydrz@me.com>
2018-11-08 19:17:08 -0500

    adds BinaryTreeValidation tests and BinaryTreeNode class

--- BinaryTreeValidation/BinartTreeValidation.spec.js
+++ BinaryTreeValidation/BinartTreeValidation.spec.js
@@ -1,9 +1,27 @@
-const { binaryTreeValidation } = require('./BinaryTreeValidation');
+const {
+  binaryTreeValidation,
+  BinaryTreeNode,
+} = require('./BinaryTreeValidation');
+
+const head = new BinaryTreeNode(50);
+head.insertLeft(30).insertLeft(20);
+head.left.insertRight(60);
+head.insertRight(80).insertLeft(70);
+head.right.insertRight(90);
 
 describe('Binary Tree Validation Testing Suite', () => {
   it('should return a boolean', () => {
     expect(
       typeof binaryTreeValidation({ value: 20, left: null, right: null })
     ).toBe('boolean');
+  });
+  it('should return false', () => {
+    expect(binaryTreeValidation(head)).toBe(false);
+  });
+  it('should return true', () => {
+    expect(binaryTreeValidation(head.left)).toBe(true);
+  });
+  it('should return true', () => {
+    expect(binaryTreeValidation(head.right)).toBe(true);
   });
 });
--- BinaryTreeValidation/BinaryTreeValidation.js
+++ BinaryTreeValidation/BinaryTreeValidation.js
@@ -3,4 +3,21 @@ function binaryTreeValidation(node) {
 }
 
 // Do not modify code below this comment
-module.exports = { binaryTreeValidation }
+class BinaryTreeNode {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+
+  insertLeft(value) {
+    this.left = new BinaryTreeNode(value);
+    return this.left;
+  }
+
+  insertRight(value) {
+    this.right = new BinaryTreeNode(value);
+    return this.right;
+  }
+}
+module.exports = { binaryTreeValidation, BinaryTreeNode };
