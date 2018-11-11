10d4edcf1d985546a367c7a081c29c37af867798
AJ Genung <DragonRydrz@me.com>
2018-11-10 19:33:21 -0500

    AlternatingCaracters started

---
+++ AlternatingCharacters/AlternatingCharacters.js
@@ -0,0 +1,20 @@
+/* 
+
+  Complete the alternatingCharacters function in the editor below. 
+  It must return an integer representing the minimum number of deletions 
+  to make the alternating string.
+
+  AAAA      Returns 3
+  BBBBB     Returns 4
+  ABABABAB  Returns 0
+  BABABA    Returns 0
+  AAABBB    Returns 4
+
+*/
+
+const alternatingCharacters = str => {
+  // Your code here
+}
+
+// Do not modify code below this line
+module.exports = { alternatingCharacters }
