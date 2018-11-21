56b4ca1cc4337a16bdd06f3d4c9ef0c483ae419c
AJ Genung <DragonRydrz@me.com>
2018-11-20 19:51:28 -0500

    LinkedListClass template

---
+++ LinkedListClass/LinkedListClass.js
@@ -0,0 +1,46 @@
+/*
+  Build out a LinkedList class and a Node class.
+  Functions to impliment are listed in the template.
+  Complete code so all methods function as expected.
+*/
+
+class LinkedList {
+  constructor(value) {
+    // Your code here
+  }
+
+  addToHead = value => {
+    // Add new Node to the head
+    // Your code here
+  };
+
+  addToTail = value => {
+    // Add new Node to the tail
+    // Your code here
+  };
+
+  removeFromHead = () => {
+    // Remove the node from the head
+    // Your code here
+  };
+
+  removeFromTail = () => {
+    // Remove the node from the tail
+    // Your code here
+  };
+
+  length = () => {
+    // Return the total number of nodes in the linked list
+    // Your Code Here
+  };
+}
+
+class Node {
+  constructor(value) {
+    // Value and Next properties required
+    // Your code here
+  }
+}
+
+// Do not modify code below this comment
+module.exports = { LinkedList, Node };
