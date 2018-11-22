729b25fe24895f9485920937c6a75d0112ee6108
AJ Genung <DragonRydrz@me.com>
2018-11-22 08:05:21 -0500

    LinkedListClass code completed

--- LinkedListClass/LinkedListClass.js
+++ LinkedListClass/LinkedListClass.js
@@ -8,38 +8,76 @@ class LinkedList {
   constructor(value) {
     // Head property required
     // Your code here
+    this.head = new Node(value);
   }
 
   addToHead = value => {
     // Add new Node to the head
     // Your code here
+    if (!this.head) {
+      this.head = new Node(value, this.head);
+      return;
+    }
+    const newNode = new Node(value, this.head);
+    this.head = newNode;
   };
 
   addToTail = value => {
     // Add new Node to the tail
     // Your code here
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.addToHead(value);
+    } else {
+      let current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = newNode;
+    }
   };
 
   removeFromHead = () => {
     // Remove the node from the head
     // Your code here
+    if (!this.head) return;
+    const cache = this.head;
+    this.head = this.head.next;
+    return cache;
   };
 
   removeFromTail = () => {
     // Remove the node from the tail
     // Your code here
+    if (!this.head.next) this.removeFromHead();
+    let current = this.head;
+    while (current.next.next) {
+      current = current.next;
+    }
+    const cache = current.next;
+    current.next = null;
+    return cache;
   };
 
   length = () => {
     // Return the total number of nodes in the linked list
     // Your Code Here
+    let counter = 0;
+    let current = this.head;
+    while (current) {
+      counter++;
+      current = current.next;
+    }
+    return counter;
   };
 }
 
 class Node {
-  constructor(value) {
+  constructor(value, next = null) {
     // Value and Next properties required
     // Your code here
+    this.value = value;
+    this.next = next;
   }
 }
 
