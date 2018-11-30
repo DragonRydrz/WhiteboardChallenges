749a94971d395ce99187b8dcc5e134ecaca77cb0
AJ Genung <DragonRydrz@me.com>
2018-11-29 21:33:21 -0500

    LeftRotation started

---
+++ LeftRotation/LeftRotation.js
@@ -0,0 +1,15 @@
+/*
+  A left rotation operation on an array shifts each of the array's elements  
+  unit to the left. `leftRotation` will recieve 2 arguments.  First will be an array,
+  second will be the number of left shifts that need to happen.
+
+  If the array passed in is [1, 2, 3, 4, 5],
+  and the number of shifts is 3,
+  the output should be [4,5,1,2,3]
+*/
+
+const leftRotation = () => {
+  // Your code here
+};
+
+module.exports = { leftRotation };
