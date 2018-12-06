0846c5eb19b3069f197c073c4bcfad8a89379e7a
AJ Genung <DragonRydrz@me.com>
2018-12-05 22:54:36 -0500

    One Letter Difference set up

---
+++ OneLetterDifference/OneLetterDifference.js
@@ -0,0 +1,16 @@
+/*
+  Write a function that takes an array of strings.  In that array, there are 2 strings that differ by only one character.
+  Remove the mismatched character, and return the characters that are the same in the 2 strings.
+  Example:
+  Input = ['qwerty', 'qwedty', 'fjeisd', 'eidspc', 'gjdeis', 'padstn']
+  'qwerty' and 'qwedty' have 5 out of 6 matching characters (in the same position).
+  Return = 'qwety'
+  If none exist, return false.
+*/
+
+function oneLetterDifference() {
+  // Your code here
+}
+
+// Do not modify code below this line
+module.exports = { oneLetterDifference };
