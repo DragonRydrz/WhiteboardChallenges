a3c1854c05cf54dc084b89ada276e6568d1605ea
AJ Genung <DragonRydrz@me.com>
2018-12-08 17:34:01 -0500

    Can Be Palindrome set up with started problem

---
+++ CanBePalindrome/CanBePalindrome.js
@@ -0,0 +1,13 @@
+/*
+  Check to see if a string can be arranged to be a palindrome. (Identical frowards and backwards).
+  Return true or false.
+  'aaabbeedd' should return true.  Can be rearranged to make 'abedadeba' which is a palindrome.
+  'aaabbb' should return false.  This can not be rearranged into a Palindrome
+*/
+
+function canBePalindrome(str) {
+  // Your code here
+}
+
+// Do not modify code below this comment
+module.exports = { canBePalindrome };
