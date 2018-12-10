c8797842c2b1e023eba2ef400ea5b7bcecc5bc08
AJ Genung <DragonRydrz@me.com>
2018-12-10 07:36:41 -0500

    Can Be Palindrome adds 2 test cases

--- CanBePalindrome/CanBePalindrome.spec.js
+++ CanBePalindrome/CanBePalindrome.spec.js
@@ -4,4 +4,10 @@ describe('Can Be Palindrome Testing Suite', () => {
   it('should return true or false', () => {
     expect(typeof canBePalindrome('test')).toBe('boolean');
   });
+  it('should return true', () => {
+    expect(canBePalindrome('aaabbeedd')).toBe(true);
+  });
+  it('should return true', () => {
+    expect(canBePalindrome('aaabbb')).toBe(true);
+  });
 });
